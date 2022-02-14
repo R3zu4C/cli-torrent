c44044124959caf6b3e249872ffec9e4181e5552
R3zu4C <a.patanwal@protonmail.com>
2022-02-14 22:16:13 +0530

    added block support

--- src/torrent-parser.js
+++ src/torrent-parser.js
@@ -3,7 +3,9 @@ import bencode from 'bencode';
 import crypto from 'crypto';
 import bignum from 'bignum';
 
-const open = (filepath) => fs.readFileSync(filepath);
+const BLOCK_LEN = 2 ** 14;
+
+const open = (filepath) => bencode.decode(fs.readFileSync(filepath));
 
 const infoHash = (torrent) => {
   const info = bencode.encode(torrent.info);
@@ -23,4 +25,32 @@ const size = (torrent) => {
   return bignum.toBuffer(torrentSize, { endian: 'big', size: 8 });
 };
 
-export { open, infoHash, size };
+const pieceLen = (torrent, pieceIndex) => {
+  const totalLength = bignum.fromBuffer(size(torrent)).toNumber();
+  const pieceLength = torrent.info['piece length'];
+
+  const lastPieceLength = totalLength % pieceLength;
+  const lastPieceIndex = Math.floor(totalLength / pieceLength);
+
+  if (lastPieceIndex === pieceIndex) return lastPieceLength;
+  return pieceLength;
+};
+
+const blocksPerPiece = (torrent, pieceIndex) => {
+  const pieceLength = pieceLen(torrent, pieceIndex);
+  return Math.ceil(pieceLength / BLOCK_LEN);
+};
+
+const blockLen = (torrent, pieceIndex, blockIndex) => {
+  const pieceLength = pieceLen(torrent, pieceIndex);
+
+  const lastBlockLength = pieceLength % BLOCK_LEN;
+  const lastBlockIndex = Math.floor(pieceLength / BLOCK_LEN);
+
+  if (blockIndex === lastBlockIndex) return lastBlockLength;
+  return BLOCK_LEN;
+};
+
+export default {
+  BLOCK_LEN, open, infoHash, size, pieceLen, blocksPerPiece, blockLen,
+};
